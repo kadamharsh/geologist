82de457a5adf2aa760de46e76d62ddaff0388774
kadamharsh <76954925+kadamharsh@users.noreply.github.com>
2021-04-28 06:57:24 -0700

    Add files via upload

---
+++ sand.js
@@ -0,0 +1,27 @@
+class Sand{
+constructor(x, y,r) 
+{
+    var options = {
+      'density':3,
+      'friction': 5,
+      'restitution':0.3
+    };
+    this.x=x;
+    this.y=y;
+    this.r=r;
+    this.body=Bodies.circle(this.x,this.y,(this.r-20)/2,options)
+    World.add(world, this.body);
+  };
+  display(){
+
+var pos=this.body.position;
+    push();
+    translate(pos.x, pos.y);
+    rectMode(CENTER)
+    strokeWeight(4);
+    stroke('black');
+    fill('red');
+    ellipse(0,0,this.r,this.r);
+    pop();
+  }
+}
